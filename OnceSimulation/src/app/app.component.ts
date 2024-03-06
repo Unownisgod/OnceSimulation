9c1d38fe096a1de16d8928577c88c899e4985830
David Pérez Cruz <dperezc01@gmail.com>
2024-03-06 12:25:48 +0100

    simulates all 3 coupons

--- OnceSimulation/src/app/app.component.ts
+++ OnceSimulation/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { WeekDay } from '@angular/common';
 import { Component } from '@angular/core';
 interface Coupon {
   number: string
@@ -15,30 +16,107 @@ export class AppComponent {
 
   winnerCoupon!: Coupon;
   playersCoupon!: Coupon;
+
+  moneySpend!: number;
   moneyEarned!: number;
+  balance!: number;
+  date!: Date;
 
   ngOnInit() {
-    this.playersCoupon = {number: "48652", series: "40"}
-    this.GenerateCoupon();
-    console.log(this.winnerCoupon);
-    var coincidences = this.TestCoupon();
-    this.moneyEarned = this.CheckPrizes(coincidences.coincidences, coincidences.seriesCoincides)
+    this.moneyEarned = this.moneySpend = this.balance = 0
+    this.date = new Date();
+
+    setInterval(() => {
+      if (this.date.getDay() <= 4) {
+        this.NormalCoupon();
+      }
+      else if (this.date.getDay() == 5) {
+        this.FridayCoupon();
+      }
+      else {
+        this.WeekendCoupon();
+      }
+        this.balance = this.moneyEarned - this.moneySpend
+      this.date.setDate(this.date.getDate() + 1)
+    }, 1000);
   }
 
-  GenerateCoupon() {
-    var winnerCouponNumber = Math.floor(Math.random() * 99999).toFixed();
-    if (winnerCouponNumber.length < 5) {
-      while (winnerCouponNumber.length < 5) {
-         winnerCouponNumber = "0"+winnerCouponNumber
-      }
-    }
-    var winnerCouponSeries = Math.floor(Math.random() * 50).toFixed();
+
+  private NormalCoupon() {
+    this.moneySpend += 2;
+    this.playersCoupon = this.GenerateNormalCoupon();
+    this.winnerCoupon = this.GenerateNormalCoupon();
+    var coincidences = this.TestCoupon();
+    this.moneyEarned += this.CheckNormalPrizes(coincidences.coincidences, coincidences.seriesCoincides);
+  }
+
+  private FridayCoupon() {
+    this.moneySpend += 3;
+    this.playersCoupon = this.GenerateFridayCoupon();
+    this.winnerCoupon = this.GenerateFridayCoupon();
+    var coincidences = this.TestCoupon();
+    this.moneyEarned += this.CheckFridayPrizes(coincidences.coincidences, coincidences.seriesCoincides);
+  }
+
+  private WeekendCoupon() {
+    this.moneySpend += 2;
+    this.playersCoupon = this.GenerateWeekendCoupon();
+    this.winnerCoupon = this.GenerateWeekendCoupon();
+    var coincidences = this.TestCoupon();
+    this.moneyEarned += this.CheckWeekendPrizes(coincidences.coincidences, coincidences.seriesCoincides)
+  }
+
+  GenerateNormalCoupon(): Coupon {
+    var winnerCouponNumber = this.generateCouponNumber();
+    var winnerCouponSeries = Math.floor(1+Math.random() * 50).toFixed();
     if (winnerCouponSeries.length < 3) {
       while (winnerCouponSeries.length < 3) {
         winnerCouponSeries = "0" + winnerCouponSeries
       }
     }
-    this.winnerCoupon = { number: winnerCouponNumber, series: winnerCouponSeries }
+    return { number: winnerCouponNumber, series: winnerCouponSeries }
+  }
+
+  private generateCouponNumber() {
+    var winnerCouponNumber = Math.floor(Math.random() * 99999).toFixed();
+    if (winnerCouponNumber.length < 5) {
+        while (winnerCouponNumber.length < 5) {
+            winnerCouponNumber = "0" + winnerCouponNumber;
+        }
+    }
+    return winnerCouponNumber;
+  }
+
+  GenerateFridayCoupon(): Coupon {
+    var winnerCouponNumber = Math.floor(Math.random() * 99999).toFixed();
+    if (winnerCouponNumber.length < 5) {
+      while (winnerCouponNumber.length < 5) {
+        winnerCouponNumber = "0" + winnerCouponNumber
+      }
+    }
+    var winnerCouponSeries = Math.floor(1 + Math.random() * 135).toFixed();
+    if (winnerCouponSeries.length < 3) {
+      while (winnerCouponSeries.length < 3) {
+        winnerCouponSeries = "0" + winnerCouponSeries
+      }
+    }
+    return { number: winnerCouponNumber, series: winnerCouponSeries }
+  }
+
+  GenerateWeekendCoupon(): Coupon {
+    var winnerCouponNumber = Math.floor(Math.random() * 99999).toFixed();
+    if (winnerCouponNumber.length < 5) {
+      while (winnerCouponNumber.length < 5) {
+        winnerCouponNumber = "0" + winnerCouponNumber
+      }
+    }
+    var winnerCouponSeries = Math.floor(1 + Math.random() * 55).toFixed();
+    if (winnerCouponSeries.length < 3) {
+      while (winnerCouponSeries.length < 3) {
+        winnerCouponSeries = "0" + winnerCouponSeries
+      }
+    }
+    return { number: winnerCouponNumber, series: winnerCouponSeries }
   }
 
   TestCoupon(): { coincidences: number, seriesCoincides: boolean } {
@@ -52,23 +130,24 @@ export class AppComponent {
       }
     }
     var reverseCoincidences = 0;
-    for (let i = 0; i > 5; i++) {
-      if (this.winnerCoupon.number.charAt(i) == this.playersCoupon.number.charAt(i)) {
-        reverseCoincidences++;
+    if (this.date.getDay() >= 6) {
+      for (let i = 0; i > 5; i++) {
+        if (this.winnerCoupon.number.charAt(i) == this.playersCoupon.number.charAt(i)) {
+          reverseCoincidences++;
+        }
+        else {
+          break
+        }
       }
-      else {
-        break
+      if (coincidences < reverseCoincidences) {
+        coincidences = reverseCoincidences;
       }
-    }
-    if (coincidences < reverseCoincidences) {
-      coincidences = reverseCoincidences;
     }
     var seriesCoincides = this.playersCoupon.series == this.winnerCoupon.series
-    console.log(coincidences + ", " + seriesCoincides)
     return { coincidences, seriesCoincides }
   }
 
-  CheckPrizes(coincidences: number, seriesCoincides: boolean): number {
+  CheckNormalPrizes(coincidences: number, seriesCoincides: boolean): number {
     switch (true) {
       case coincidences == 5 && seriesCoincides:
         return 500000;
@@ -80,6 +159,44 @@ export class AppComponent {
         return 25;
       case coincidences == 2:
         return 6;
+      case coincidences == 1:
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  CheckFridayPrizes(coincidences: number, seriesCoincides: boolean): number {
+    switch (true) {
+      case coincidences == 5 && seriesCoincides:
+        return 6000000;
+      case coincidences == 5:
+        return 40000;
+      case coincidences == 4:
+        return 500;
+      case coincidences == 3:
+        return 50;
+      case coincidences == 2:
+        return 6;
+      case coincidences == 1:
+        return 3;
+      default:
+        return 0;
+    }
+  }
+
+  CheckWeekendPrizes(coincidences: number, seriesCoincides: boolean): number {
+    switch (true) {
+      case coincidences == 5 && seriesCoincides:
+        return 1500000;
+      case coincidences == 5:
+        return 20000;
+      case coincidences == 4:
+        return 200;
+      case coincidences == 3:
+        return 30;
+      case coincidences == 2:
+        return 4;
       case coincidences == 1:
         return 2;
       default:
